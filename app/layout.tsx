8811a4e7b654c794a467adbd2b7a24bef8dc8ab9
justnooob <71315508+justnooob@users.noreply.github.com>
2026-07-25 23:18:22 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,6 +111,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ru" data-theme="dark">
       <body>
+        <!-- Yandex.Metrika counter -->
+          <script type="text/javascript">
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=111031642', 'ym');
+          
+              ym(111031642, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          </script>
+          <noscript><div><img src="https://mc.yandex.ru/watch/111031642" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+          <!-- /Yandex.Metrika counter -->
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(personJsonLd) }}
