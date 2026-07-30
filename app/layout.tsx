f526c1ccec7749ecf43ed2bfe30bfc70d067fbe2
justnooob <71315508+justnooob@users.noreply.github.com>
2026-07-30 17:24:01 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -135,6 +135,20 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(structuredData),
           }}
+
+          <script type="text/javascript">
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=111031642', 'ym');
+          
+              ym(111031642, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          </script>
+          <noscript><div><img src="https://mc.yandex.ru/watch/111031642" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+          <!-- /Yandex.Metrika counter -->
+
         />
 
         <AppProvider>
@@ -147,49 +161,4 @@ export default function RootLayout({
           <ScrollToTop />
         </AppProvider>
 
-        <Script id="yandex-metrika" strategy="lazyOnload">
-          {`
-(function(m,e,t,r,i,k,a){
-m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-m[i].l=1*new Date();
-for (var j=0;j<document.scripts.length;j++){
-if(document.scripts[j].src===r){return;}
-}
-k=e.createElement(t),
-a=e.getElementsByTagName(t)[0],
-k.async=1,
-k.src=r,
-a.parentNode.insertBefore(k,a);
-})(window,document,'script',
-'https://mc.yandex.ru/metrika/tag.js?id=111031642','ym');
 
-ym(
-111031642,
-'init',
-{
-  ssr:true,
-  webvisor:true,
-  clickmap:true,
-  trackLinks:true,
-  accurateTrackBounce:true,
-  ecommerce:"dataLayer",
-});
-`}
-        </Script>
-
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/111031642"
-              style={{
-                position: 'absolute',
-                left: '-9999px',
-              }}
-              alt=""
-            />
-          </div>
-        </noscript>
-      </body>
-    </html>
-  );
-}
